7d11331aecb434e704e14e86338be0dec0a482e4
Smetan-dot <smetanaandrei@gmail.com>
2023-04-20 22:55:10 +0300

    feat: add js-generation keys on keyboard

--- index.js
+++ index.js
@@ -24,3 +24,27 @@ function init() {
 }
 
 init();
+
+const keyboardContainer = document.querySelector(".keyboard-container");
+const keyboardKeys = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", 
+                      "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "'\'", "Del", 
+                      "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", 
+                      "Lshift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "Rshift", 
+                      "Lctrl", "Win", "Lalt", " ", "Ralt", "◄", "▼", "►", "Rctrl"];
+
+function createKeys() {
+    keyboardKeys.forEach((element) => {
+        const keyElement = document.createElement("div");
+        keyElement.innerHTML = element;
+        keyElement.className = "key";
+        if(element === "Backspace" || element === "CapsLock" || element === "Enter" ||
+           element === "Lshift" || element === "Rshift") keyElement.classList.add("wide");
+        if(element === "Tab" || element === "Del" || element === "Lctrl" || element === "Rctrl" || 
+           element === "Lalt" || element === "Ralt" || element === "Win" || element === "▲" || 
+           element === "◄" || element === "▼" || element === "►") keyElement.classList.add("dark");
+        if(element === " ") keyElement.classList.add("super-wide");
+        keyboardContainer.appendChild(keyElement);
+    })
+}
+
+createKeys();
