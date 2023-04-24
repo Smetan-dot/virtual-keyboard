4b39ccd8ec0d93b4d8da74b7ae97f772540eaec8
Smetan-dot <smetanaandrei@gmail.com>
2023-04-24 18:35:19 +0300

    feat: add shift+ layouts and give some functionality

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function init() {
     document.body.appendChild(systemDescription);
 
     const languageDescription = document.createElement("p");
-    languageDescription.textContent = "Для переключения языка комбинация: левыe shift + alt";
+    languageDescription.textContent = "Для переключения языка комбинация: левыe ctrl + alt";
     languageDescription.className = "description";
     document.body.appendChild(languageDescription);
 }  // creating form for virtual keyboard
@@ -42,7 +42,7 @@ const keyboardKeysEnShift = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", "
                         "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "Enter", 
                         "Lshift", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "▲", "Rshift", 
                         "Lctrl", "Win", "Lalt", " ", "Ralt", "◄", "▼", "►", "Rctrl"];
-const keyboardKeysRuUpShift = ["Ё", "!", "\"", "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "Backspace", 
+const keyboardKeysRuShift = ["Ё", "!", "\"", "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "Backspace", 
                         "Tab", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "/", "Del", 
                         "CapsLock", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Enter", 
                         "Lshift", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",", "▲", "Rshift", 
@@ -63,33 +63,10 @@ window.addEventListener('load', getLocalStorage()); // get language
 
 function createKeys() {
     if(language === undefined) language = "en";
-    keyboardKeysEn.forEach((element) => {
-        const keyElement = document.createElement("div");
-        keyElement.textContent = element;
-        keyElement.className = "key";
-        keyElement.classList.add("en");
-        if(element === "Backspace" || element === "CapsLock" || element === "Enter" ||
-           element === "Lshift" || element === "Rshift") keyElement.classList.add("wide");
-        if(element === "Tab" || element === "Del" || element === "Lctrl" || element === "Rctrl" || 
-           element === "Lalt" || element === "Ralt" || element === "Win" || element === "▲" || 
-           element === "◄" || element === "▼" || element === "►") keyElement.classList.add("dark");
-        if(element === " ") keyElement.classList.add("super-wide");
-        keyboardContainer.appendChild(keyElement);
-    })
-
-    keyboardKeysRu.forEach((element) => {
-        const keyElement = document.createElement("div");
-        keyElement.textContent = element;
-        keyElement.className = "key";
-        keyElement.classList.add("ru");
-        if(element === "Backspace" || element === "CapsLock" || element === "Enter" ||
-           element === "Lshift" || element === "Rshift") keyElement.classList.add("wide");
-        if(element === "Tab" || element === "Del" || element === "Lctrl" || element === "Rctrl" || 
-           element === "Lalt" || element === "Ralt" || element === "Win" || element === "▲" || 
-           element === "◄" || element === "▼" || element === "►") keyElement.classList.add("dark");
-        if(element === " ") keyElement.classList.add("super-wide");
-        keyboardContainer.appendChild(keyElement);
-    })
+    createLayout(keyboardKeysEn, "en");
+    createLayout(keyboardKeysRu, "ru");
+    createLayout(keyboardKeysEnShift, "enShift");
+    createLayout(keyboardKeysRuShift, "ruShift");
 
     if(language === "en") document.querySelectorAll(".ru").forEach((el) => {
         el.classList.add("hidden");
@@ -97,50 +74,87 @@ function createKeys() {
     if(language === "ru") document.querySelectorAll(".en").forEach((el) => {
         el.classList.add("hidden");
     })
+
+    document.querySelectorAll(".enShift").forEach((el) => {
+        el.classList.add("hidden");
+    })
+    document.querySelectorAll(".ruShift").forEach((el) => {
+        el.classList.add("hidden");
+    })
 }  // creating buttons in keyboard-container (2 languages)
+
+function createLayout(data, type) {
+    data.forEach((element) => {
+    const keyElement = document.createElement("div");
+        keyElement.textContent = element;
+        keyElement.className = "key";
+        keyElement.classList.add(type);
+        if(element === "Backspace" || element === "CapsLock" || element === "Enter" ||
+           element === "Lshift" || element === "Rshift") keyElement.classList.add("wide");
+        if(element === "Tab" || element === "Del" || element === "Lctrl" || element === "Rctrl" || 
+           element === "Lalt" || element === "Ralt" || element === "Win" || element === "▲" || 
+           element === "◄" || element === "▼" || element === "►") keyElement.classList.add("dark");
+        if(element === " ") keyElement.classList.add("super-wide");
+        keyboardContainer.appendChild(keyElement);
+    })
+}
 
 createKeys();
 
 let textWindow = document.querySelector(".text-window");
+const keys = document.querySelectorAll(".key");
 const keysRu = document.querySelectorAll(".ru");
 const keysEn = document.querySelectorAll(".en");
+const keysRuShift = document.querySelectorAll(".ruShift");
+const keysEnShift = document.querySelectorAll(".enShift");
 
 function pressKey() {
     document.body.addEventListener("keydown", function(event) {
         event.preventDefault();
         if(language === "en") {
-            let keys = keysEn;
-            actionsForKeys(keys, event);
+            actionsForKeys(keysEn, keysEnShift, event);
         }
         if(language === "ru"){
-            let keys = keysRu;
-            actionsForKeys(keys, event);
+            actionsForKeys(keysRu, keysRuShift, event);
         }
     })
 
     document.body.addEventListener("keyup", function(event) {
         for(let j = 0; j < keys.length; j++) {
-            if(keys[j].textContent === event.key && keys[j].textContent !== "CapsLock") keys[j].classList.remove("press");
+            if(keys[j].textContent !== "CapsLock") keys[j].classList.remove("press");
             if(keys[j].textContent === "Lshift" && event.code === "ShiftLeft" || 
-               keys[j].textContent === "Rshift" && event.code === "ShiftRight" || 
-               keys[j].textContent === "Lalt" && event.code === "AltLeft" || 
-               keys[j].textContent === "Ralt" && event.code === "AltRight" || 
-               keys[j].textContent === "Lctrl" && event.code === "ControlLeft" || 
-               keys[j].textContent === "Rctrl" && event.code === "ControlRight" || 
-               keys[j].textContent === "Del" && event.code === "Delete" || 
-               keys[j].textContent === "Win" && event.code === "MetaLeft" || 
-               keys[j].textContent === "▲" && event.code === "ArrowUp" || 
-               keys[j].textContent === "◄" && event.code === "ArrowLeft" || 
-               keys[j].textContent === "▼" && event.code === "ArrowDown" || 
-               keys[j].textContent === "►" && event.code === "ArrowRight") keys[j].classList.remove("press");
+               keys[j].textContent === "Rshift" && event.code === "ShiftRight") {
+                   if(language === "en") {
+                       keysEnShift.forEach((el) => {
+                           el.classList.add("hidden");
+                       })
+                       keysEn.forEach((el) => {
+                           el.classList.remove("hidden");
+                       })
+                   }
+                   if(language === "ru") {
+                       keysRuShift.forEach((el) => {
+                           el.classList.add("hidden");
+                    })
+                       keysRu.forEach((el) => {
+                           el.classList.remove("hidden");
+                    })
+                }
+            }
         }
     })
 }    // physical press keys on keyboard
 
 pressKey();
 
-function actionsForKeys(keys, event) {
+function actionsForKeys(keys, data, event) {
     for(let i = 0; i < keys.length; i++) {
+        if(keys[i].textContent === event.key.toLowerCase() && keys[i].textContent !== "Enter" && 
+           keys[i].textContent !== "CapsLock" && keys[i].textContent !== "Backspace" && 
+           keys[i].textContent !== "Tab" && keys[i].textContent !== " " && event.shiftKey) {
+               keys[i].classList.add("press");
+               textWindow.textContent += event.key;
+           }
         if(keys[i].textContent === event.key && keys[i].textContent !== "Enter" && 
            keys[i].textContent !== "CapsLock" && keys[i].textContent !== "Backspace" && 
            keys[i].textContent !== "Tab") {
@@ -163,8 +177,16 @@ function actionsForKeys(keys, event) {
             textWindow.textContent = textWindow.textContent.slice(0, -1);
         }
         if(keys[i].textContent === "Lshift" && event.code === "ShiftLeft" || 
-           keys[i].textContent === "Rshift" && event.code === "ShiftRight" || 
-           keys[i].textContent === "Lalt" && event.code === "AltLeft" || 
+           keys[i].textContent === "Rshift" && event.code === "ShiftRight"){
+               keys[i].classList.add("press");
+               keys.forEach((el) => {
+                   el.classList.add("hidden");
+               })
+               data.forEach((el) => {
+                   el.classList.remove("hidden");
+               })
+           } 
+        if(keys[i].textContent === "Lalt" && event.code === "AltLeft" || 
            keys[i].textContent === "Ralt" && event.code === "AltRight" || 
            keys[i].textContent === "Lctrl" && event.code === "ControlLeft" || 
            keys[i].textContent === "Rctrl" && event.code === "ControlRight" || 
@@ -180,8 +202,6 @@ function actionsForKeys(keys, event) {
     }
 } // TODO: win and del functionality
 
-const keys = document.querySelectorAll(".key");
-
 function clickOnKey() {
     for(let i = 0; i < keys.length; i++) {
         keys[i].addEventListener("mousedown", function() {
@@ -191,10 +211,27 @@ function clickOnKey() {
             else if(keys[i].textContent === "Backspace") textWindow.textContent = textWindow.textContent.slice(0, -1);
             else if(keys[i].textContent === "Tab") textWindow.textContent += "    ";
             else if(keys[i].textContent === "CapsLock" || keys[i].textContent === "Win" || 
-                    keys[i].textContent === "Lshift" || keys[i].textContent === "Rshift" || 
                     keys[i].textContent === "Lalt" || keys[i].textContent === "Ralt" || 
                     keys[i].textContent === "Lctrl" || keys[i].textContent === "Rctrl" || 
                     keys[i].textContent === "Del");
+            else if(keys[i].textContent === "Lshift" || keys[i].textContent === "Rshift") {
+                if(language === "en") {
+                    keysEnShift.forEach((el) => {
+                        el.classList.remove("hidden");
+                    })
+                    keysEn.forEach((el) => {
+                        el.classList.add("hidden");
+                    })
+                }
+                if(language === "ru") {
+                    keysRuShift.forEach((el) => {
+                        el.classList.remove("hidden");
+                    })
+                    keysRu.forEach((el) => {
+                        el.classList.add("hidden");
+                    })
+                }
+            }
             else textWindow.textContent += keys[i].textContent;
         })
     }
@@ -202,6 +239,24 @@ function clickOnKey() {
     for(let j = 0; j < keys.length; j++) {
         keys[j].addEventListener("mouseup", function() {
             if(keys[j].textContent !== "CapsLock") keys[j].classList.remove("press");
+            if(keys[j].textContent === "Lshift" || keys[j].textContent === "Rshift") {
+                if(language === "en") {
+                    keysEnShift.forEach((el) => {
+                        el.classList.add("hidden");
+                    })
+                    keysEn.forEach((el) => {
+                        el.classList.remove("hidden");
+                    })
+                }
+                if(language === "ru") {
+                    keysRuShift.forEach((el) => {
+                        el.classList.add("hidden");
+                    })
+                    keysRu.forEach((el) => {
+                        el.classList.remove("hidden");
+                    })
+                }
+            }
         })
     }
 }    // mouse-click at virtual keyboard
@@ -248,4 +303,4 @@ function checkPressedKeys(func, ...codes) {
       });
 } // change language 
 
-checkPressedKeys(changeLanguage, "AltLeft", "ShiftLeft");
+checkPressedKeys(changeLanguage, "AltLeft", "ControlLeft");
